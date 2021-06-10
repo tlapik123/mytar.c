301ea9e6d55d7aea105fb18bd5a521f838acf097
Petr Tlapa <tlapik123@gmail.com>
2021-06-10 21:31:05 +0200

    Added function to check if there was any content in a block.

--- main.c
+++ main.c
@@ -1,9 +1,13 @@
 #include <stdio.h>
+#include <stdbool.h>
 
 
+#define NAME_SIZE 100
+#define BLOCK_SIZE 512
+#define PART_SIZE (BLOCK_SIZE/ sizeof(long long))
 
-struct posix_header
-{                              /* byte offset */
+
+struct posix_header {                              /* byte offset */
     char name[100];               /*   0 */
     char mode[8];                 /* 100 */
     char uid[8];                  /* 108 */
@@ -23,7 +27,67 @@ struct posix_header
     /* 500 */
 };
 
+/**
+ * Check if one BLOCK_SIZEd block has content.
+ * @param file_handle File to read block from.
+ * @return
+ * 0: No content was read.
+ * Else: Some content was read;
+ */
+unsigned long long read_block_for_content(FILE *file_handle) {
+    unsigned long long content = 0;
+    // Read 512 block - one small part at a time to tell if its empty
+    int how_many_times = PART_SIZE;
+    while (how_many_times > 0) {
+        unsigned long long tmp_content;
+        fread(&tmp_content, PART_SIZE, 1, file_handle);
+        --how_many_times;
+        content |= tmp_content;
+    }
+    return content;
+}
+
 int main(int argc, char *argv[]) {
-    printf("Hello, World!\n");
+    if (argc < 2) {
+        printf("No arguments were supplied\n");
+        return 1;
+    }
+    // Open a tar file.
+    FILE *tar_file = fopen(argv[1], "rb");
+
+    // Read name of the first file there.
+    char name[NAME_SIZE];
+    fgets(name, NAME_SIZE, tar_file);
+    printf("%s\n", name);
+
+    // Seek to second block.
+    int was_successful = fseek(tar_file, BLOCK_SIZE, SEEK_SET);
+
+
+    // Have some enum later to tell if 2 blocks were without content.
+    int blocks_without_content = 0;
+    unsigned long long content = 0;
+    while (blocks_without_content != 2){
+        content |= read_block_for_content(tar_file);
+        if (content == 0){
+            ++blocks_without_content;
+        } else{
+            // zero out content
+            content = 0;
+        }
+    }
+
+    bool block_read_flag = false;
+
+
+
+
+
+    // If 2 consecutive blocks empty read another name
+
+    // Repeat.
+
+
+
     return 0;
 }
