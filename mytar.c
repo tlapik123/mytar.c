2706cadb8039f839ab555c1053d9452d5b296ce6
Petr Tlapa <tlapik123@gmail.com>
2021-06-13 16:42:58 +0200

    0.96 Added function prototypes and parse function.

--- mytar.c
+++ mytar.c
@@ -3,6 +3,7 @@
 #include <assert.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdarg.h>
 
 
 #define BLOCK_SIZE 512
@@ -10,7 +11,19 @@
 #define ONE 1
 #define ARRAY_SIZE(x) (sizeof(x) / sizeof((x)[0]))
 #define SUPPORTED_OPTION_COUNT 2
+#define PROGRAM_NAME "mytar"
+#define EOF_ERR PROGRAM_NAME": Unexpected EOF in archive\n"
+#define NON_RECOVERABLE_ERR PROGRAM_NAME": Error is not recoverable: exiting now\n"
 
+
+static inline size_t number_of_content_blocks(char size_as_string[]);
+
+__attribute__((noreturn))
+static void my_errx(int return_code, char return_string[], int va_count, ...);
+
+static bool is_block_empty(void *block);
+
+static void my_dispose(FILE *file_to_close, void *memory_to_free);
 
 /**
  * Header struct.
@@ -36,23 +49,37 @@ typedef struct {                    /* byte offset */
     /* 512 */
 } whole_header;
 
-size_t number_of_content_blocks(char size_as_string[]) {
-    long size_as_long = strtol(size_as_string, NULL, OCTAL);
-    // Size should never be negative.
-    assert(size_as_long >= 0);
-
-    size_t size = (size_t) size_as_long;
-    return (size / BLOCK_SIZE) + ((size % BLOCK_SIZE == 0) ? 0 : 1);
+static inline size_t number_of_content_blocks(char size_as_string[]) {
+    size_t size_as_long = strtoul(size_as_string, NULL, OCTAL);
+    return (size_as_long / BLOCK_SIZE) + ((size_as_long % BLOCK_SIZE == 0) ? 0 : 1);
 }
 
-// TODO: some attribute to say that we never return from this func?
-void my_errx(int return_code, char return_string[]) {
-    printf(return_string);
+/**
+ * THIS FUNCTION NEVER RETURNS!
+ *
+ * Formats and prints error message to stderr and exits with specified return code.
+ * @param return_code Code to exit with.
+ * @param return_string String to write to stderr.
+ * @param va_count Number of variable arguments.
+ * @param ... Arguments format the string with.
+ */
+static void my_errx(int return_code, char return_string[], int va_count, ...) {
+    va_list args;
+    va_start(args, va_count);
+    // print error message to stderr.
+    vfprintf(stderr, return_string, args);
+    va_end(args);
     exit(return_code);
 }
 
-
-bool is_block_empty(void *block) {
+/**
+ * Checks if the given block is empty.
+ * @param block Block to check.
+ * @return
+ * True: Block is empty.
+ * False: Block is not empty.
+ */
+static bool is_block_empty(void *block) {
     unsigned long long result = 0;
     unsigned int repeat_count = BLOCK_SIZE / sizeof(result);
     // there shouldn't be any remainder
@@ -68,6 +95,7 @@ bool is_block_empty(void *block) {
     return result == 0 ? true : false;
 }
 
+// TODO: remove this.
 int get_num_of_digits(long num) {
     int digits = 0;
     while (num > 0) {
@@ -77,7 +105,20 @@ int get_num_of_digits(long num) {
     return digits;
 }
 
+/**
+ * Closes the file and frees the memory.
+ * @param file_to_close File in need of closing.
+ * @param memory_to_free Memory in need of freeing.
+ */
+static void my_dispose(FILE *file_to_close, void *memory_to_free) {
+    free(memory_to_free);
+    int close_res = fclose(file_to_close);
+    if (close_res == EOF) my_errx(2, "File wasn't successfully closed!\n", 0);
+}
 
+/**
+ * What option is active
+ */
 enum active_option {
     NONE,
     F,
@@ -86,22 +127,27 @@ enum active_option {
     X
 };
 
-int main(int argc, char *argv[]) {
-    if (argc < 4) {
-        my_errx(2, "Need at least 3 arguments.\n");
+/**
+ * Parse command line arguments.
+ * @param argc Number of actual arguments (without program name).
+ * @param argv Arguments to parse.
+ * @param filename Pointer to Filename to return to.
+ * @param t_names_actual_length Actual lenght of t_name array.
+ * @param t_names Array of 't' option file names.
+ */
+static void
+parse_options(int argc, char *argv[], const char **filename, int *t_names_actual_length, const char *t_names[]) {
+    if (argc < 3) {
+        my_errx(2, "Need at least 3 arguments.\n", 0);
     }
-    char *filename = NULL;
     // Current active option.
     enum active_option active_o = NONE;
     // 0th index:'f', 1st index:'t'
-    // char option_decoder[] = {'f', 't'};
+    char option_decoder[] = {'f', 't'};
     bool encountered_options[SUPPORTED_OPTION_COUNT] = {false};
-    // 't' option files
-    const char *t_names[argc - 3];
-    int t_names_actual_length = 0;
-    for (int i = 1; i < argc; ++i) {
+    for (int i = 0; i < argc; ++i) {
         if (argv[i][0] == '-') {
-            if (strlen(argv[i]) != 2) my_errx(2, "Long option encountered.\n");
+            if (strlen(argv[i]) != 2) my_errx(2, "Long option %s encountered.\n", 1, argv[i]);
             switch (argv[i][1]) {
                 case 'f':
                     encountered_options[0] = true;
@@ -112,26 +158,26 @@ int main(int argc, char *argv[]) {
                     active_o = T;
                     continue;
                 default:
-                    my_errx(2, "Unknown option.\n");
+                    my_errx(2, "Unknown option.\n", 0);
             }
         }
         switch (active_o) {
             case F:
-                filename = argv[i];
+                *filename = argv[i];
                 // If there was a 't' switch back to it.
                 active_o = encountered_options[1] ? T : NONE;
                 break;
             case T:
                 // Add search name to the list.
-                t_names[t_names_actual_length] = argv[i];
-                ++t_names_actual_length;
+                t_names[*t_names_actual_length] = argv[i];
+                ++(*t_names_actual_length);
                 break;
             case V:
             case X:
-                my_errx(2, "Option not implemented.\n");
-                assert(false);
+                my_errx(2, "Option not implemented.\n", 0);
             case NONE:
-                my_errx(2, "Option missing!\n");
+                my_errx(2, "Option missing!\n", 0);
+            default:
                 assert(false);
         }
     }
@@ -139,21 +185,29 @@ int main(int argc, char *argv[]) {
     // Check option dependency
     for (size_t i = 0; i < ARRAY_SIZE(encountered_options); ++i) {
         if (!encountered_options[i]) {
-            my_errx(2, "Some option wasn't specified.\n");
+            my_errx(2, "Option %c wasn't specified.\n", 1, option_decoder[i]);
         }
     }
+}
+
+
+int main(int argc, char *argv[]) {
+    int t_names_actual_length = 0;
+    const char *filename = NULL;
+    const char *t_names[argc - 3];
+    parse_options(--argc, ++argv, &filename, &t_names_actual_length, t_names);
 
     bool appearance[t_names_actual_length];
 
     // Open a tar file.
     FILE *tar_file = fopen(filename, "rb");
     // Some problem with file.
-    if (tar_file == NULL) my_errx(2, "File couldn't be opened/wasn't found.\n");
+    if (tar_file == NULL) my_errx(2, "File couldn't be opened/wasn't found.\n", 0);
     // Malloc space for header
     whole_header *header = malloc(sizeof(whole_header));
     if (header == NULL) {
         fclose(tar_file);
-        my_errx(2, "Malloc returned NULL.\n");
+        my_errx(2, "Malloc returned NULL.\n", 0);
     }
 
     // Number of empty blocks encountered.
@@ -169,21 +223,14 @@ int main(int argc, char *argv[]) {
         if (header_read_res != sizeof(*header)) {
             // We reached EOF and there was only one empty block.
             if (empty_block_count != 0) {
-                free(header);
-                fclose(tar_file);
-                const char *non_formatted = "mytar: A lone zero block at %zu\n";
-                const int formatted_len = (int) strlen(non_formatted) + get_num_of_digits((long) blocks_so_far);
-                char formatted[formatted_len];
-                sprintf(formatted, non_formatted, (blocks_so_far + 1));
-                my_errx(0, formatted);
+                my_dispose(tar_file, header);
+                my_errx(0, PROGRAM_NAME": A lone zero block at %zu\n", 1, (blocks_so_far + 1));
             }
             // We reached EOF without 2 empty blocks.
             if (header_read_res == 0) break;
 
-            free(header);
-            fclose(tar_file);
-            my_errx(2, "mytar: Unexpected EOF in archive\n"
-                       "mytar: Error is not recoverable: exiting now\n");
+            my_dispose(tar_file, header);
+            my_errx(2, EOF_ERR NON_RECOVERABLE_ERR, 0);
         }
 
         // Optimization - only if name is empty we check if block was empty.
@@ -192,28 +239,20 @@ int main(int argc, char *argv[]) {
                 ++empty_block_count;
                 continue;
             }
-            free(header);
-            fclose(tar_file);
-            my_errx(2, "Non recognizable header.\n");
+            my_dispose(tar_file, header);
+            my_errx(2, "Non recognizable header.\n", 0);
         }
 
-        // TODO: we encountered empty block but there wasn't a second one or EOF
+        // We encountered empty block but there wasn't a second one or EOF
         if (empty_block_count != 0) {
-            // TODO: should we return?
-            free(header);
-            fclose(tar_file);
-            my_errx(0, "A lone zero block encountered.\n");
+            my_dispose(tar_file, header);
+            my_errx(0, PROGRAM_NAME": A lone zero block at %zu\n", 1, (blocks_so_far + 1));
         }
 
         // We only care about regular files.
         if (header->typeflag != '0' && header->typeflag != 0) {
-            free(header);
-            fclose(tar_file);
-            const char *non_formatted = "mytar: Unsupported header type: %d\n";
-            const int formatted_len = (int) strlen(non_formatted) + 4;
-            char formatted[formatted_len];
-            sprintf(formatted, non_formatted, header->typeflag);
-            my_errx(2, formatted);
+            my_dispose(tar_file, header);
+            my_errx(2, PROGRAM_NAME": Unsupported header type: %d\n", 1, header->typeflag);
         }
 
         if (t_names_actual_length != 0) {
@@ -242,11 +281,8 @@ int main(int argc, char *argv[]) {
         size_t skipped_res = fread(tmp_block, ONE, size_to_skip, tar_file);
         // We reached the EOF sooner than we should.
         if (skipped_res != size_to_skip) {
-            // TODO: hopefully right?
-            free(header);
-            fclose(tar_file);
-            my_errx(2, "mytar: Unexpected EOF in archive\n"
-                       "mytar: Error is not recoverable: exiting now\n");
+            my_dispose(tar_file, header);
+            my_errx(2, EOF_ERR NON_RECOVERABLE_ERR, 0);
         }
         // Increment number of blocks encountered.
         blocks_so_far += (blocks_to_skip + 1);
@@ -254,11 +290,10 @@ int main(int argc, char *argv[]) {
     bool all_files_found = true;
     for (size_t i = 0; i < ARRAY_SIZE(appearance); ++i) {
         if (!appearance[i]) {
-            printf("mytar: %s: Not found in archive\n", t_names[i]);
+            fprintf(stderr, PROGRAM_NAME": %s: Not found in archive\n", t_names[i]);
             all_files_found = false;
         }
     }
-    free(header);
-    fclose(tar_file);
-    if (!all_files_found) my_errx(2, "mytar: Exiting with failure status due to previous errors\n");
+    my_dispose(tar_file, header);
+    if (!all_files_found) my_errx(2, PROGRAM_NAME": Exiting with failure status due to previous errors\n", 0);
 }
